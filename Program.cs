30630bc1cf6638c32eb21dd240d88ac5671af8d7
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2023-10-04 20:43:18 +0300

    feat: clear code

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-﻿using CS_Labs;
-
-new CaesarCipher();
-
-/*
- message = beginattacknow
- kw = cryptography
- k = 3
- d = tabelpvvpoilmz
- 
- 
- message = beginattacknow
- or cryptogram = D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
- k = 3
- d = EHJLQDWWDFNQRZ
-*/
+﻿return;
